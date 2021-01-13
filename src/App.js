9bace7b9a77c8e4dfda8530869aed133d78d3f54
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-13 17:54:12 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Dropdown from "react-dropdown";
+import parse from "html-react-parser";
+
+import { formatSeasons } from "./utils/formatSeasons";
+import { fetchShow } from "./api/fetchShow";
+
+import Episodes from "./components/Episodes";
+import "./styles.css";
+
+export default function App() {
+  const [show, setShow] = useState(null);
+  const [seasons, setSeasons] = useState([]);
+  const [selectedSeason, setSelectedSeason] = useState("");
+  const episodes = seasons[selectedSeason] || [];
+
+  useEffect(() => {
+    fetchShow().then(showData => {
+      setShow(showData);
+      setSeasons(formatSeasons(showData._embedded.episodes));
+    });
+  }, []);
+
+  const handleSelect = e => {
+    setSelectedSeason(e.value);
+  };
+
+  if (!show) {
+    return <h2>Fetching data...</h2>;
+  }
+
+  return (
+    <div className="App">
+      <img className="poster-img" src={show.image.original} alt={show.name} />
+      <h1>{show.name}</h1>
+      {parse(show.summary)}
+      <Dropdown
+        options={Object.keys(seasons)}
+        onChange={handleSelect}
+        value={selectedSeason || "Select a season"}
+        placeholder="Select an option"
+      />
+      <Episodes episodes={episodes} />
+    </div>
+  );
+}
